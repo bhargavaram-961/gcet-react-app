7fd8e033ec63438bd23e52b4e8cf1dcf138bb2fe
bhargavaram-961 <bhargavaram961@gmail.com>
2025-05-22 18:20:31 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import './App.css';
+import Home from './Components/Home';
+import Products from './Components/Products';
+import Login from './Components/Login';
 
 function App() {
   return (
-    <>
-    <header>Ecommerce:By Bhargava Ram</header>
-    <hr />
-      <h1>My React STORE</h1>
-      <hr />
-      <footer>&copyright; all rights reserved</footer>
-    </>
-  )
+    <BrowserRouter>
+      <header>
+        <h1>ECommerce: By Tarun</h1>
+        <nav>
+          <Link to="/" style={{ marginRight:'10px' }}>Home</Link>
+          <Link to="/products" style={{ marginRight:'10px' }}>Products</Link>
+          <Link to="/login">Login</Link>
+        </nav>
+        <hr />
+      </header>
+
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </main>
+
+      <footer>
+        <hr />
+        <h1>Footer</h1>
+      </footer>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
