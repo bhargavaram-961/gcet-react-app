ea0e4531bc7f6c96103cc2e2d1005c6fb62a8733
bhargavaram-961 <bhargavaram961@gmail.com>
2025-06-03 09:18:50 +0530

    h

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,34 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import './App.css';
-import Home from './Components/Home';
-import Products from './Components/Products';
-import Login from './Components/Login';
-
+import { createContext, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Cart from "./Components/Cart";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Login from "./Components/Login";
+import Logout from "./Components/Logout";
+import Products from "./Components/Products";
+import Register from "./Components/Register";
+export const AppContext = createContext();
 function App() {
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState({});
   return (
-    <BrowserRouter>
-      <header>
-        <h1>ECommerce: By  Bhargava Ram</h1>
-        <nav>
-          <Link to="/" style={{ marginRight:'10px' }}>Home</Link>
-          <Link to="/products" style={{ marginRight:'10px' }}>Products</Link>
-          <Link to="/login">Login</Link>
-        </nav>
-        <hr />
-      </header>
-
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </main>
-
-      <footer>
-        <hr />
-        <h1>Footer</h1>
-      </footer>
-    </BrowserRouter>
+    <div>
+      <AppContext.Provider value={{ users, setUsers, user, setUser }}>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route index element={<Products />} />
+            <Route path="/" element={<Products />}></Route>
+            <Route path="/cart" element={<Cart />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/logout" element={<Logout />}></Route>
+            <Route path="/register" element={<Register />}></Route>
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </AppContext.Provider>
+    </div>
   );
 }
-
 export default App;
